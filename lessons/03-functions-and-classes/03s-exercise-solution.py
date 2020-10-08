c0994e09a636fc1e7d519cd4642334d6792f6393
Tyler Bettilyon <teb@tebs-lab.com>
2020-10-07 19:15:05 -0600

    fixed a bug in the 03 exerise solution

--- lessons/03-functions-and-classes/03s-exercise-solution.py
+++ lessons/03-functions-and-classes/03s-exercise-solution.py
@@ -21,8 +21,8 @@ def median(input_list):
         return None
     # Even case
     elif n % 2 == 0:
-        a = sorted_list[n / 2]
-        b = sorted_list[n / 2 - 1]
+        a = sorted_list[n // 2]
+        b = sorted_list[n // 2 - 1]
         return (a + b) / 2
     # Odd case
     else:
@@ -37,7 +37,7 @@ print(m) # 10
 
 class Averages:
     def __init__(self, input_data):
-        self.data = input_data
+        self.data = input_data.copy()
 
     def mean(self):
         return sum(self.data) / len(self.data) # replace with your code
@@ -51,8 +51,8 @@ class Averages:
             return None
         # Even case
         elif n % 2 == 0:
-            a = sorted_list[n / 2]
-            b = sorted_list[n / 2 - 1]
+            a = sorted_list[n // 2]
+            b = sorted_list[n // 2 - 1]
             return (a + b) / 2
         # Odd case
         else:
@@ -61,6 +61,12 @@ class Averages:
 
 
 # Sample tests, you should add more.
-avgs = Averages([5,10,15])
+l_one = [5,10,15]
+avgs = Averages(l_one)
+print(avgs.mean()) # 10
+print(avgs.median()) # 10
+
+# Prove that we made a copy of the list, no side effects.
+l_one.append(25)
 print(avgs.mean()) # 10
 print(avgs.median()) # 10
